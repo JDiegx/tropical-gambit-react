f97408f807f1da3a2692fa26e6ef9e95cff664d6
Juan Diego Sanchez Martinez <juandiego.sanchez@globant.com>
2024-08-05 01:38:26 -0500

    Bug fixes and improvements with stronger authentication

--- src/components/tropical-gambit-sales/tropical-sales.jsx
+++ src/components/tropical-gambit-sales/tropical-sales.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
 
 const TropicalSales = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            navigate('/');
+        } catch (error) {
+            console.error('Error al cerrar sesión:', error);
+        }
+    };
+
     return (
         <div>
             <h1>Welcome Tropical</h1>
+            <button onClick={handleLogout}>Cerrar sesión</button>
         </div>
-    )
-}
+    );
+};
 
 export default TropicalSales;
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,11 +1,9 @@
-// src/routers/AppRouter.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebaseConfig';
 import Layout from '../components/layout';
 import TropicalSales from '../components/tropical-gambit-sales/tropical-sales';
-import ErrorPage from '../components/errorPages';
 
 const AppRouter = () => {
     const [user, loading] = useAuthState(auth);
@@ -13,13 +11,15 @@ const AppRouter = () => {
         return null;
     }
 
+    const isAuthenticated = !!user;
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />} />
                 <Route
                     path="/sales"
-                    element={user ? <TropicalSales /> : <ErrorPage />}
+                    element={isAuthenticated ? <TropicalSales /> : <Navigate to="/" replace />}
                 />
                 <Route path="*" element={<Navigate to="/" replace />} /> {/* Redirige cualquier ruta desconocida */}
             </Routes>
